06d03456a104c3a900224060d1ce5112229dbe77
leeyongseok13 <minamiharuka333@gmail.com>
2023-09-21 16:59:43 +0900

    20230921

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
 
 import "./App.css";
-import TopBanner from "./component/TopBanner";
 import Header from "./component/Header";
 
 class App extends Component {
   render() {
     return (
       <>
-        <TopBanner />
         <Header />
       </>
     );
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,29 +1,30 @@
-import { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="Header">
-        <div className="container">
-          <h1>
-            <a href="/">
-              <img
-                src={process.env.PUBLIC_URL + "./image/logo.jpg"}
-                alt="KBO_Bingo"
-              ></img>
-            </a>
-          </h1>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-          </ul>
-        </div>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <Navbar bg="light" data-bs-theme="light">
+      <Container fluid>
+        <Navbar.Brand href="#home">KBO_Bingo</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#team">팀</Nav.Link>
+          <Nav.Link href="#players">선수</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="검색"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">검색</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
